f34eacb8bd27c761e95621b017ef3c1ee3f653d0
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-11-07 20:03:58 -0300

    primeiro contato com javascript e variaveis

---
+++ aula1.js
@@ -0,0 +1,19 @@
+var nome = "Tiago"
+
+var grausCelcius = 10
+var grausFh = (grausCelcius * 1.8) + 32
+
+var emReais = 100
+var emDolar = emReais / 5.06
+
+
+var valorFinal = emDolar.toFixed (2)
+
+
+console.log("Bem vindo " + nome)
+console.log(grausFh)
+console.log(valorFinal)
+
+
+// revisão 
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
