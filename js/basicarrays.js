a310139bad196401e458407446ec52fa187795e2
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-11-08 00:40:55 -0300

    primeiro contato com arrays

---
+++ js/basicarrays.js
@@ -0,0 +1,12 @@
+var listaFilmes = ["Yesterday", " A Chegada", "Escola de Rock"];
+
+listaFilmes.push("Harry Potter 2");
+
+listaFilmes.push("Harry Potter 3");
+
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+  document.write("<p>" + listaFilmes[indice] + "</p>");
+}
+
+// revisão
+// array, document.write, indice, push, for (;), ++, .length,
