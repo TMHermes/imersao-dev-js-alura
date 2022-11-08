e4f1ba3972603b7c5392872c0f04084ca63b6969
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-11-08 00:53:06 -0300

    treinando push em arrays

---
+++ js/aluraflix-v1.js
@@ -0,0 +1,13 @@
+var listaFilmes = [
+    "https://cdn.abcdoabc.com.br/Django-Livre-1_809e0fab.jpg",
+    "https://cosmonerd.com.br//uploads/2020/12/leonardo-dicaprio-o-lobo-de-wall-street.jpg",
+    "https://f.i.uol.com.br/fotografia/2020/09/04/15992275065f52467226ff9_1599227506_3x2_md.jpg"
+  ];
+  
+  for (var indice = 0; indice < listaFilmes.length; indice++) {
+    document.write("<img src=" + listaFilmes[indice] + ">");
+  }
+  
+  // revisão
+  // array, document.write, indice, push, for (;), ++, .length, img, src=
+  
